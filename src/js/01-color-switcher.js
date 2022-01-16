f19c955eb2ce9f399e49908ae96a012998f546cc
Bronislav Dereniuk <d.bronuk.m@gmail.com>
2022-01-16 14:27:37 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', changeRondomColor)
+stopBtn.addEventListener('click', stopChangeRondomColor);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+let rondomColor = null;
+
+function changeRondomColor() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+     rondomColor = setInterval(()=>{
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+
+function stopChangeRondomColor() {
+    startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(rondomColor
+  )
+};
+
+// stopBtn.addEventListener('click', () => {
+//   startBtn.disabled = false;
+//   stopBtn.disabled = true;
+//   clearInterval(rondomColor)});
